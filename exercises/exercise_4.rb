283386757e6a737504a74e40deac2917da33c6c7
zezking <zhaoenze001@gmail.com>
2021-03-29 14:56:44 -0700

    finish exercise 3

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,8 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey=Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel:true)
+whistler=Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel:false)
+yaletown=Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel:true)
+
+puts Store.count
