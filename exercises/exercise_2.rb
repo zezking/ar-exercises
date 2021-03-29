af22a59c22ff8760d2cfeeb31efe3f6a20a11e19
zezking <zhaoenze001@gmail.com>
2021-03-29 14:51:16 -0700

    finish exercise2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1=Store.find_by(id:1)
+@store2=Store.find_by(id:2)
+
+@store1.name="Lougheed"
+
+@store1.save
+
+puts  @store1.name
