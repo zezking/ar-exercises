dead1673aa105807da616bbf634e85b764ef03d1
zezking <zhaoenze001@gmail.com>
2021-03-29 15:25:21 -0700

    finish exercise 6

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ActiveRecord::Base
-
+    belongs_to :store
+    @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+    @store1.employees.create(first_name: "Sori", last_name: "Han", hourly_rate: 100)
+    @store1.employees.create(first_name: "Sam", last_name: "Yeppers", hourly_rate: 100)
+    @store2.employees.create(first_name: "Haha", last_name: "Shiba", hourly_rate: 50)
+    @store2.employees.create(first_name: "Josh", last_name: "FBoy", hourly_rate: 30)
+    @store2.employees.create(first_name: "Eric", last_name: "Codboy", hourly_rate: 40)
 end
